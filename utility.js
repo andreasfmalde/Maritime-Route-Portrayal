c66bfcb62aacc7689ab43f792d8537458fe7591f
andreasfmalde <andreas.malde@lyse.net>
2024-06-21 16:04:37 +0200

    add: angle calculation between to route legs

--- utility.js
+++ utility.js
@@ -27,3 +27,24 @@ export function recursiveNamespaceTrimmer(obj){
         }
     }
 }
+
+// Calculate the angle between two legs defined by 3 waypoints
+// IMPORTANT: The second parameter is the waypoint where the legs meet
+function calculateRouteLegAngle(wp1=[0,2],wp2=[-1,0],wp3=[0,-2]){
+    let leg1 = [wp1[0]-wp2[0],wp1[1]-wp2[1]];
+    let leg2 = [wp3[0]-wp2[0],wp3[1]-wp2[1]];
+
+    const dotProduct = leg1[0]*leg2[0] + leg1[1]*leg2[1];
+    const leg1Magnitude = Math.sqrt(leg1[0]**2 + leg1[1]**2);
+    const leg2Magnitude = Math.sqrt(leg2[0]**2 + leg2[1]**2);
+
+    const angle = Math.acos(dotProduct/(leg1Magnitude*leg2Magnitude));
+
+    const degree =  angle * (180/Math.PI);
+
+    console.log(`Angle: ${degree} degrees | ${angle} radians`);
+
+}
+
+
+calculateRouteLegAngle([10,-2],[8,0],[10,4]);
