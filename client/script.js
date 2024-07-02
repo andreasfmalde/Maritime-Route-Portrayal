3cd377240908c21f978a19d15ff24039f8750e39
andreasfmalde <andreas.malde@lyse.net>
2024-07-02 10:56:36 +0200

    add: actionpoint point implementation and symbols incoorporation

--- client/script.js
+++ client/script.js
@@ -1,6 +1,7 @@
 import {route} from './route.js';
 const style = {
     "version": 8,
+    "glyphs": "https://demotiles.maplibre.org/font/{fontstack}/{range}.pbf",
       "sources": {
       "osm": {
               "type": "raster",
@@ -24,8 +25,8 @@ const style = {
 const map = new maplibregl.Map({
     container: 'map', // container id
     style: style, // style URL
-    center: [5.7, 59], // starting position [lng, lat]
-    zoom: 10 // strting zoom
+    center: [20, 57.5], // starting position [lng, lat]
+    zoom: 5 // strting zoom
 })
 
 let geojson;
@@ -46,7 +47,16 @@ try{
 }
 
 
-map.on('load', () => {
+map.on('load', async () => {
+
+
+    const image = await map.loadImage('assets/1x/RTEWPT01.png');
+    const image2 = await map.loadImage('assets/1x/RTEACTPOT01.png');
+
+    map.addImage('wpt-img', image.data);
+    map.addImage('act-img', image2.data);
+
+
     map.addSource('geojsonSource', {
         'type': 'geojson',
         'data': geojson
@@ -70,16 +80,51 @@ map.on('load', () => {
     }); 
 
     map.addLayer({
-        'id': 'geojsonLayer',
-        'type': 'circle',
+        'id': 'waypoint-symbol',
+        'type': 'symbol',
         'source': 'geojsonSource',
-        'filter': ['==', '$type', 'Point'],
-        'paint': {
-            'circle-radius': 3,
-            'circle-color': '#B42222'
+        'filter': ['==', 'type', 'waypoint'],
+        'layout': {
+            'icon-image': 'wpt-img',
+            'icon-size': 0.5
         }
-    }); 
 
+    });
+    map.addLayer({
+        'id':'actionpoint-symbol',
+        'type': 'symbol',
+        'source': 'geojsonSource',
+        'filter': ['==', 'type', 'actionpoint'],
+        'layout': {
+            'icon-image': 'act-img',
+            'icon-size': 0.6,
+            'text-field': ['get', 'routeActionPointRequiredActionDescription'],
+            'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+            'text-size': 10,
+            'text-anchor': 'bottom-left',
+            'text-offset': [0, 2],
+        },
+        'paint': {
+            'text-color': '#AA44A8',
+            'text-opacity': 0.5,
+        }
+    });
+
+    /*
+    map.addLayer({
+        'id': 'text-layer',
+        'type': 'symbol',
+        'source': 'geojsonSource',
+        'layout': {
+          'text-field': "Hello",
+          "text-font": ["Open Sans Regular", "Arial Unicode MS Regular"],
+          "text-size": 12
+        },
+        'paint': {
+          'text-color': '#000000'
+        },
+        'filter': ['==', '$type', 'Point'],
+      });*/
     
 
 });
